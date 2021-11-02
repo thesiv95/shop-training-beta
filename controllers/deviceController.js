cd4fd1eb247cf001b4f6caaf367c3d5f15f578f0
Ilya Soloveychik <dj.active.17@gmail.com>
2021-11-02 17:20:46 +0200

    feat(devices): add filter, pagination and count in getAll route

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -34,8 +34,31 @@ class DeviceController {
 
     async getDevices(req, res, next) {
         try {
-            const devices = await Device.findAll();
-            return res.status(200).json({ devices });
+            const {
+                typeId,
+                brandId,
+                page = 1,
+                perPage = 10 // limit
+            } = req.query;
+
+            const offset = page * perPage - perPage;
+
+            let filterQuery;
+
+            if (!typeId && !brandId) filterQuery = {};
+            if (typeId && !brandId) filterQuery = { where: { typeId } };
+            if (!typeId && brandId) filterQuery = { where: { brandId } };
+            if (typeId && brandId) filterQuery = { where: { typeId, brandId } };
+
+            const [devices, devicesQuantity] = await Promise.all([
+                Device.findAll({
+                    ...filterQuery,
+                    limit: perPage,
+                    offset,
+                }),
+                Device.count(filterQuery),
+            ]);
+            return res.status(200).json({ devices, devicesQuantity });
         } catch (error) {
             return next(ApiError.internal(error));
         }
