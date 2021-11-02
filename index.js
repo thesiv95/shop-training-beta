1e1ff4dd91efa6776276419bbcfe202c0159a5a7
Ilya Soloveychik <dj.active.17@gmail.com>
2021-11-02 09:53:32 +0200

    feat: dB connection added

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('init');
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./db');
+const models = require('./models/models');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.status(200).json({ 'works': true }));
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+start();
